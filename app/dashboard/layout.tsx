2cd1efd0c6652be7fd8064c01f73ebbf813ffab1
Amirmahdi-Monzavi <amirmahdimonzavi@gmail.com>
2024-08-05 17:48:18 +0330

    refactor(layout): remove header margin button

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <header className="bg-red-500 text-center mb-4 p-4">
-        Dashboard Layout
-      </header>
+      <header className="bg-red-500 text-center  p-4">Dashboard Layout</header>
       {children}
     </section>
   );
