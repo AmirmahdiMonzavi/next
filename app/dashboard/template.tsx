f113ae5e0bc1f8b5ce8cdddfb8e981844430a27d
Amirmahdi-Monzavi <amirmahdimonzavi@gmail.com>
2024-08-05 18:05:40 +0330

    feat(template): add navigation links to dashboard template

--- app/dashboard/template.tsx
+++ app/dashboard/template.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ChangeEvent, useState } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const [value, setValue] = useState<string>();
+  const pathname = usePathname();
 
   const changeInputValue = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.currentTarget.value);
@@ -20,6 +23,28 @@ export default function Template({ children }: { children: React.ReactNode }) {
           placeholder="Enter somthing then change the route"
           className="px-4 py-2 w-[400px] border-2 border-black"
         />
+        <div>
+          <Link
+            href="/dashboard"
+            className={`block ${
+              pathname === "/dashboard"
+                ? "font-bold text-blue-200"
+                : "text-white underline"
+            }`}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/dashboard/settings"
+            className={`block ${
+              pathname === "/dashboard/settings"
+                ? "font-bold text-blue-200"
+                : "text-white underline"
+            }`}
+          >
+            Settings
+          </Link>
+        </div>
       </header>
       {children}
     </section>
