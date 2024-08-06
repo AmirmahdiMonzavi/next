b848c92656a16148c479a4c2786f6fe0853e37d8
Amirmahdi <amirmahdikaren@gmail.com>
2024-08-06 09:53:29 +0330

    feat(routes): modify content with suspense fallback

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1 className="text-center">Dashboard Page</h1>;
+import { Suspense } from "react";
+
+import Loading from "./loading";
+
+const textPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Resolved Text");
+    }, 2000);
+  });
+};
+
+export default async function Page() {
+  const text = await textPromise();
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <h1 className="text-center">{text as string}</h1>
+    </Suspense>
+  );
 }
